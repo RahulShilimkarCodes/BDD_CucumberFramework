bbdb4cb2553da3322443a74b9e06dfc57ab86e57
Rahul Shilimkar <silimkarr@gmail.com>
2026-06-13 00:33:02 +0530

    added re-run,extent reports and screenshots

--- src/test/java/greenKartSteps/Hooks.java
+++ src/test/java/greenKartSteps/Hooks.java
@@ -1,10 +1,22 @@
 package greenKartSteps;
 
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import testUtils.TestSetup;
+import testUtils.WebDriverManager;
+
 
 public class Hooks {
 
@@ -21,6 +33,32 @@ public class Hooks {
 		testSetup.webDriverManager.initDriver();			//since constructor does new TestSetup() in background, we can all methods as well...
 		testSetup.initializeWaits();
 		testSetup.initializePageObjects();
+	}
+	
+	@AfterStep
+	public void attachScreenshot(Scenario scenario) throws IOException
+	{
+		 WebDriver driver = WebDriverManager.getDriver();
+		 if (scenario.isFailed()) {
+		        TakesScreenshot ts = (TakesScreenshot) driver;
+		        File screenshot = ts.getScreenshotAs(OutputType.FILE);
+
+		      //converting File screenshot to Byte to, use to attach to extent reports..
+		        byte[] screenshotBytes = FileUtils.readFileToByteArray(screenshot);
+		        scenario.attach(screenshotBytes, "image/png", "Failure Screenshot");
+		    }
+	}
+	
+	@AfterStep
+	public void attachAllStepsScreenshot(Scenario scenario) throws IOException
+	{
+		WebDriver driver = WebDriverManager.getDriver();
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File screenshot = ts.getScreenshotAs(OutputType.FILE);
+
+		// converting File screenshot to Byte to, use to attach to extent reports..
+		byte[] screenshotBytes = FileUtils.readFileToByteArray(screenshot);
+		scenario.attach(screenshotBytes, "image/png", "Failure Screenshot");	    
 	}
 	
 	@After
--- src/test/java/testRunners/CucumberTestRunner.java
+++ src/test/java/testRunners/CucumberTestRunner.java
@@ -6,16 +6,25 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features="classpath:FeatureFile/placeOrder.feature",
+		features="classpath:FeatureFile/searchProduct.feature",
+				//features="classpath:FeatureFile", 	-- to run all the feature files...
 		glue="greenKartSteps",
 		dryRun=false,
 		monochrome=true,
-		plugin= {"pretty", "html:target/greenkart-cucumber.html","json:target/greenkart-cucumber.json"},
-		tags="@EndToEnd"
+		plugin= {"pretty", "html:target/greenkart-cucumber.html","json:target/greenkart-cucumber.json",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"rerun:target/failed_scenarios.txt"},
+		tags="@DataDriven"		
 		)
 
 
 public class CucumberTestRunner extends AbstractTestNGCucumberTests{
+	
+	
+	static {
+		System.setProperty("extent.properties",
+			    System.getProperty("user.dir") + "src/test/resources/propertyFiles/extent.properties");
+    }
 	
 	@Override						//since same method name in child class...
 	@DataProvider(parallel=true)
@@ -23,6 +32,14 @@ public class CucumberTestRunner extends AbstractTestNGCucumberTests{
 	{
 		return super.scenarios();
 	}
+	
+	/*
+	 * To use extent spark in CI-CD, use below
+	 * Command Line ---->     mvn test -Dextent.properties=src/test/resources/propertyFiles/extent.properties
+	 * Class ---> System.getProperty("extent.properties", "extent.properties")
+	 * 
+	 * 
+	 */
 	
 	/*
 	 * @DataProvider
